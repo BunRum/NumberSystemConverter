1c4787001a3ec5afba6c7aa2bcd28b00acc128fa
BunRum <61174957+BunRum@users.noreply.github.com>
2026-09-03 14:46:45 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <cmath>
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+
+int main() {
+
+    cout << "====================================" << endl;
+    cout << "      NUMBER SYSTEM CONVERTER" << endl;
+    cout << "====================================" << endl;
+
+    cout << "1. Binary to Decimal" << endl;
+    cout << "2. Decimal to Binary" << endl;
+    cout << "3. Decimal to Hexadecimal" << endl;
+    cout << "4. Hexadecimal to Decimal" << endl;
+    cout << "5. Binary to Hexadecimal" << endl;
+    cout << "6. Hexadecimal to Binary" << endl;
+    cout << "7. Exit" << endl;
+
+    int choice;
+    cout << "Enter your choice: ";
+    cin >> choice;
+
+    int input;
+    cout << "Enter your input: ";
+    cin >> input;
+
+    return 0;
+  }
