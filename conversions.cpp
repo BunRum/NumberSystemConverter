2838487ee317ba28c6c43f22bc45f4ebb2015cc4
bunrum <zbunrum@gmail.com>
2026-09-08 14:47:36 -0700

    added 1, 2, 3, 5

---
+++ conversions.cpp
@@ -0,0 +1,71 @@
+//
+// Created by av53503 on 9/8/2026.
+//
+#include "conversions.h"
+
+#include <format>
+#include <iostream>
+#include <cmath>
+
+// 1
+int BinaryToDecimal(int inputBinary) {
+    int quotient = inputBinary;
+    int inDecimal = 0;
+    int step = 0;
+
+    do {
+        const int remainder = quotient % 10;
+        quotient /= 10;
+        inDecimal += remainder * static_cast<int>(pow(2, step));
+        step++;
+    } while (quotient != 0);
+
+    return inDecimal;
+}
+// 2
+int DecimalToBinary(int initialInput) {
+    int input = initialInput;
+    int quotient;
+    int time = 0;
+    int inBinary = 0;
+
+    do {
+        quotient = input / 2;
+        const int remainder = input % 2;
+        inBinary += remainder * static_cast<int>(pow(10, time));
+        input = quotient;
+        time++;
+    }
+    while (quotient != 0);
+
+    return inBinary;
+}
+
+// 3
+std::string DecimalToHexadecimal(int input) {
+    std::string inHex = std::format("{:X}", input);
+    return inHex;
+}
+
+// 4
+std::string HexadecimalToDecimal(const std::string& input) {
+
+    return "";
+}
+
+// 5
+std::string BinaryToHexadecimal(const int input) {
+
+    // converts it from Binary to Decimal
+    const int toDecimal = BinaryToDecimal(input);
+
+    // convert from decimal to hexadecimal
+    std::string toHexadecimalString = DecimalToHexadecimal(toDecimal);
+
+    return toHexadecimalString;
+}
+// 6
+std::string HexadecimalToBinary(const std::string& input) {
+
+    return "";
+}
